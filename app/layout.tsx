b9b65570a41b50816b877cd54aaa15e637683b20
Emmanuel Opara (Dr.) <chimaopra@yahoo.com>
2026-09-13 20:22:26 -0700

    Load homepage remix styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./remix.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://theboringproduct.com"),
